732a54d43360374a4fa8323652f3eda8a2c9a938
Kamaal Farah <kamaal.f1@gmail.com>
2019-08-24 16:17:27 +0200

    Initial commit

---
+++ build/precache-manifest.f6db463d03aae182c698b0d519e9b67a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54ff2f0d86ab9a82e7163150a9926beb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d185aab7a539186e4e1",
+    "url": "/static/css/main.d1f00572.chunk.css"
+  },
+  {
+    "revision": "0b14a6e482ddbb423ba9",
+    "url": "/static/js/2.a3f0826e.chunk.js"
+  },
+  {
+    "revision": "9d185aab7a539186e4e1",
+    "url": "/static/js/main.01854050.chunk.js"
+  },
+  {
+    "revision": "621b9aae57bb6c73c87c",
+    "url": "/static/js/runtime~main.7326d8dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
