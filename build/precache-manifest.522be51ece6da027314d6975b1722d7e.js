f7a087e821b83c6af26b8ba7c1f29ee362256f19
Kamaal Farah <kamaal.f1@gmail.com>
2019-08-24 18:31:49 +0200

    Gatsby set up

---
+++ build/precache-manifest.522be51ece6da027314d6975b1722d7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "265759d3ec9a0ef3650607565ca2448c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20201786f5258bffb48c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0b14a6e482ddbb423ba9",
+    "url": "/static/js/2.a3f0826e.chunk.js"
+  },
+  {
+    "revision": "20201786f5258bffb48c",
+    "url": "/static/js/main.01854050.chunk.js"
+  },
+  {
+    "revision": "621b9aae57bb6c73c87c",
+    "url": "/static/js/runtime~main.7326d8dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
