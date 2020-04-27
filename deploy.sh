09fcba2cc5348bd33855e8b233eb194f343272b3
Kamaal Farah <kamaal.f1@gmail.com>
2020-04-27 11:09:48 +0200

    Deploying to github pages

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,4 @@ npx next export
 touch out/.nojekyll
 touch out/CNAME
 echo \"kamaal111.github.io\" >> out/CNAME
-# git add out/
-# git commit -m \"Deploy Next.js to gh-pages\"
-# git subtree push --prefix out origin gh-pages
-gh-pages -b master -d build
+npx gh-pages -b master -d out
