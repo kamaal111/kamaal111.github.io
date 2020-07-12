73a2b233511a39816190e5680826344f830de0d3
Kamaal Farah <kamaal.f1@gmail.com>
2020-07-12 16:36:55 +0200

    Modifying deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ npm run build
 npx next export
 touch out/.nojekyll
 touch out/CNAME
-echo $'http://www.kamaal.io\nhttp://kamaal.io' >> out/CNAME
+echo $'kamaal.io' >> out/CNAME
 npx gh-pages -b master -d out
