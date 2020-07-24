e9ee112b94ad85615e6004c63557aae906f94a56
Kamaal Farah <kamaal.f1@gmail.com>
2020-07-24 08:46:28 +0200

    Copying associated domain file to output

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ npx next export
 touch out/.nojekyll
 touch out/CNAME
 echo $'kamaal.io' >> out/CNAME
+cp -R src/public/.well-known/apple-app-site-association out/apple-app-site-association
 npx gh-pages -b master -d out
