1f05dd2f7a3ee7fb5db9fcc15d8fb00f872f4274
kamaal111 <kamaal@tranzer.com>
2020-07-24 15:46:26 +0200

    apple-app-site-association is not private

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ mkdir out/.well-known
 touch out/.nojekyll
 touch out/CNAME
 echo $'kamaal.io' >> out/CNAME
-cp -R src/public/.well-known/apple-app-site-association out/.well-known/apple-app-site-association
+cp -R src/public/.well-known/apple-app-site-association out/apple-app-site-association
 npx gh-pages -b master -d out
