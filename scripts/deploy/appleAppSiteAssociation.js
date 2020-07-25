a3268bd255c6ccf5bd603e5dd112c9e891d611fb
Kamaal Farah <kamaal.f1@gmail.com>
2020-07-25 16:29:07 +0200

    Using bot paths and components

--- scripts/deploy/appleAppSiteAssociation.js
+++ scripts/deploy/appleAppSiteAssociation.js
@@ -3,19 +3,25 @@ const { promises: fs } = require('fs');
 const { apps } = require('../config');
 
 const { appId: colorPickerAppId } = apps.ColorPicker;
+
 const colorPickerDetails = {
   appID: colorPickerAppId,
-  // paths: ['/hallo'],
+  paths: ['/hallo'],
   components: [{ '/': '/hallo' }],
 };
-const details = [colorPickerDetails];
-const applinks = { details };
 
-const main = async () => {
+const createAppleAppSiteAssociationJson = () => {
+  const details = [colorPickerDetails];
+  const applinks = { apps: [], details };
+  const appleAppSiteAssociation = { applinks };
+  const exportJson = JSON.stringify(appleAppSiteAssociation, null, 2);
+  return exportJson;
+};
+
+const exportAppleAppSiteAssociationFile = async () => {
   try {
-    const appleAppSiteAssociation = { applinks };
     const exportPath = 'out/.well-known/apple-app-site-association';
-    const exportJson = JSON.stringify(appleAppSiteAssociation, null, 2);
+    const exportJson = createAppleAppSiteAssociationJson();
     await fs.writeFile(exportPath, exportJson);
   } catch (error) {
     console.log('error', error);
@@ -23,4 +29,4 @@ const main = async () => {
   }
 };
 
-main();
+exportAppleAppSiteAssociationFile();
