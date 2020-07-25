cce9ac97c07600b5b6088ce801b52b949831b2ca
Kamaal Farah <kamaal.f1@gmail.com>
2020-07-25 16:05:35 +0200

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b867b331e6a3ed0a9523";
+/******/ 	var hotCurrentHash = "12ecfd8015dc23b8ee85";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
