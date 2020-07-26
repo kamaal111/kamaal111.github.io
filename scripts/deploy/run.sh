a0bd3004ab860d9299913331404c2ed3137b94b0
Kamaal Farah <kamaal.f1@gmail.com>
2020-07-26 15:12:57 +0200

    Adding CNAME

--- scripts/deploy/run.sh
+++ scripts/deploy/run.sh
@@ -3,9 +3,9 @@
 CURRENT_SCRIPT_PATH="scripts/deploy"
 rm -rf node_modules/.cache out
 node "$CURRENT_SCRIPT_PATH/build.js"
-
 npm run build
 npx next export
 mkdir out/.well-known
 touch out/.nojekyll
+node "$CURRENT_SCRIPT_PATH/cname.js"
 npx gh-pages -b master -d out
