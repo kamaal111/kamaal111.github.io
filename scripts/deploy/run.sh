bb87e3b451c8d637112496af6c5d97b91aad20df
Kamaal Farah <kamaal.f1@gmail.com>
2020-09-26 16:25:44 +0200

    Removing actions

--- scripts/deploy/run.sh
+++ scripts/deploy/run.sh
@@ -8,5 +8,4 @@ npx next export
 mkdir out/.well-known
 touch out/.nojekyll
 node "$CURRENT_SCRIPT_PATH/cname.js"
-git config user.name "kamaal111" && git config user.email "kamaal.f1@gmail.com"
 npx gh-pages -b master -d out
