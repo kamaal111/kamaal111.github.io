1eed6e7015cc207d17f2b7dee490852d9d46e915
Kamaal <kamaal@tranzer.com>
2022-02-15 19:47:05 +0100

    Changing copy

--- src/components/Home/HomeBanner.tsx
+++ src/components/Home/HomeBanner.tsx
@@ -4,7 +4,7 @@ function HomeBanner() {
   return (
     <div style={{ marginLeft: 16, marginRight: 16 }}>
       <h2>Kamaal Farah</h2>
-      <p>This is a society.</p>
+      <p>We live in a society.</p>
     </div>
   );
 }
