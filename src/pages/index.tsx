e16d2066f44d658f448ad8592a87663643dfcedf
Kamaal <kamaal.f1@gmail.com>
2022-03-06 12:55:29 +0100

    Importing css directly in my app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 
 import Footer from '../components/Footer';
 import HomeBanner from '../components/Home/HomeBanner';
@@ -6,6 +7,9 @@ import HomeBanner from '../components/Home/HomeBanner';
 function Home() {
   return (
     <>
+      <Head>
+        <title>Kamaal Farah</title>
+      </Head>
       <HomeBanner />
       <Footer />
     </>
