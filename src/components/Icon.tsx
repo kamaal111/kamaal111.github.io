0973d9c6183f97b1b9eab909b8a67bf035182b38
Kamaal <kamaal.f1@gmail.com>
2022-07-09 15:43:49 +0200

    Coloring icons on hover

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import classNames from '@kamaal111/classname';
 
 type Props = {
   name: string;
 };
 
 function Icon({ name }: Props) {
-  return <i className={classNames(name, 'app-link')} />;
+  return <i className={name} />;
 }
 
 export default Icon;
