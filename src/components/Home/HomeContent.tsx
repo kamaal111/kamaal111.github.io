8a6a76326089446ba4f767aeb5cc879bdb077a1a
Kamaal <kamaal.f1@gmail.com>
2022-07-10 18:46:43 +0200

    Moving avatar size to css

--- src/components/Home/HomeContent.tsx
+++ src/components/Home/HomeContent.tsx
@@ -3,23 +3,16 @@ import * as React from 'react';
 import IconLink from '../IconLink';
 
 import config from '../../config';
-import useMediaQuery from '../../hooks/useMediaQuery';
 
 const IMAGE_PATH = 'images/avatar.jpg';
 
 function HomeContent() {
-  const showMobileLayout = useMediaQuery('(max-width: 770px)');
-
-  const imageSize = showMobileLayout ? 150 : 216;
-
   return (
     <div className={'content'}>
-      {/* eslint-disable-next-line jsx-a11y/img-redundant-alt, @next/next/no-img-element */}
+      {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         className={'avatar'}
         src={IMAGE_PATH}
-        width={imageSize}
-        height={imageSize}
         alt={`Picture of ${config.fullName}`}
       />
       <h1>{config.fullName}</h1>
