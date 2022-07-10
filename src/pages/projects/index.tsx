de7e0e6c1f48a51260dd5cfa0485dc4df7da71ea
Kamaal <kamaal.f1@gmail.com>
2022-07-10 14:05:49 +0200

    Sorting projects by date

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -6,23 +6,53 @@ import Page from '../../components/Page';
 import routing from '../../../.kamaal/routing.json';
 
 function Projects() {
+  const sortedProjects = routing.projects
+    .map((project) => ({
+      ...project,
+      date: new Date(project.date),
+    }))
+    .sort((a, b) => b.date.getTime() - a.date.getTime());
+
   return (
     <Page title="Projects">
       <>
-        {routing.projects.map(({ routesPath, title, date }) => (
-          <div className="project" key={routesPath}>
-            <span className="date">{date}</span>
-
-            <div className="colored-links">
-              <Link href={routesPath}>
-                <h2>{title}</h2>
-              </Link>
-            </div>
-          </div>
+        {sortedProjects.map(({ routesPath, title, date }) => (
+          <Projects.Project
+            key={routesPath}
+            routesPath={routesPath}
+            title={title}
+            date={date}
+          />
         ))}
       </>
     </Page>
   );
 }
+
+type ProjectProps = {
+  date: Date;
+  routesPath: string;
+  title: string;
+};
+
+Projects.Project = function Project({ date, routesPath, title }: ProjectProps) {
+  return (
+    <div className="project">
+      <span className="date">
+        {date.toLocaleDateString('en-us', {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'short',
+          day: 'numeric',
+        })}
+      </span>
+      <div className="colored-links">
+        <Link href={routesPath}>
+          <h2>{title}</h2>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
