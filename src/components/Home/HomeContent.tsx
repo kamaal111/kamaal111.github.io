bd25d0bffc36b1701e1c344792e0e564c9b513df
Kamaal <kamaal.f1@gmail.com>
2022-07-10 19:02:29 +0200

    Horizontally aligning content in homescreen

--- src/components/Home/HomeContent.tsx
+++ src/components/Home/HomeContent.tsx
@@ -8,18 +8,20 @@ const IMAGE_PATH = 'images/avatar.jpg';
 
 function HomeContent() {
   return (
-    <div className={'content'}>
+    <div className={'home-content'}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         className={'avatar'}
         src={IMAGE_PATH}
         alt={`Picture of ${config.fullName}`}
       />
-      <h1>{config.fullName}</h1>
-      <h2>{config.currentRoles.join(' | ')}</h2>
-      {config.socials.map(({ icon, id, url }) => {
-        return <IconLink name={icon} url={url} key={id} />;
-      })}
+      <div className="home-info">
+        <h1>{config.fullName}</h1>
+        <h2>{config.currentRoles.join(' | ')}</h2>
+        {config.socials.map(({ icon, id, url }) => {
+          return <IconLink name={icon} url={url} key={id} />;
+        })}
+      </div>
     </div>
   );
 }
